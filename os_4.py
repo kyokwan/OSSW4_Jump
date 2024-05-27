025d8b526ddb76a79f87696164d1f2b4197da798
KyoKwan <ahnkk0904@naver.com>
2024-05-28 03:21:30 +0900

    asdf

--- os_4.py
+++ os_4.py
@@ -229,9 +229,9 @@ while running:
 
     # trick_hole 그리기
     if trick_hole_visible:
-        pygame.draw.rect(screen, WHITE, (trick_hole_x - camera_x, floor_y, 150, floor_height))  # trick_hole 크기 30으로 가정
+        pygame.draw.rect(screen, WHITE, (trick_hole_x - camera_x, floor_y, 30, floor_height)) 
     else:
-        pygame.draw.rect(screen, FLOOR_COLOR, (trick_hole_x - camera_x, floor_y, 30, floor_height))
+        pygame.draw.rect(screen, FLOOR_COLOR, (trick_hole_x - camera_x, floor_y, 150, floor_height))
 
     # 충돌 검사 및 처리
     block_collided = check_collision(character_rect, blocks)
