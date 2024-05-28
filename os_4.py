b4daad3ea3aef859883415ce2ea0cba15eba1af9
KyoKwan <ahnkk0904@naver.com>
2024-05-29 05:09:42 +0900

    속도

--- os_4.py
+++ os_4.py
@@ -306,6 +306,7 @@ while running:
     pygame.draw.rect(screen, (0, 255, 0), add_block_1.move(-camera_x, 0), 2)
     pygame.draw.rect(screen, (0, 0, 255), trigger_moving_block_zone.move(-camera_x, 0), 2)
     pygame.draw.rect(screen, (0, 255, 0), trigger_zone.move(-camera_x, 0), 2)
+    pygame.draw.rect(screen, (0, 255, 0), trigger_falling_block_zone.move(-camera_x, 0), 2)
 
     pygame.draw.rect(screen, RED, character_rect.move(-camera_x, 0))
     pygame.display.update()
