b9b81567e489bebe2cc707ed655e37f5c6037195
kyokwan <154255895+kyokwan@users.noreply.github.com>
2024-05-29 12:42:08 +0900

    #

--- os_4.py
+++ os_4.py
@@ -311,7 +311,7 @@ while running:
     for spike in spike_positions:
         pygame.draw.rect(screen, SPIKE_COLOR, (spike[0] - camera_x, spike[1], spike_width, spike_height))
 
-    # 트리거 영역 그리기
+    # 트리거 영역 그리기 # 마지막에 삭제
     pygame.draw.rect(screen, (0, 255, 0), trigger_falling_block_zone.move(-camera_x, 0), 2)
     pygame.draw.rect(screen, (0, 0, 0), del_block_1.move(-camera_x, 0), 2)
     pygame.draw.rect(screen, (0, 255, 0), add_block_1.move(-camera_x, 0), 2)
@@ -319,6 +319,8 @@ while running:
     pygame.draw.rect(screen, (0, 255, 0), trigger_zone.move(-camera_x, 0), 2)
     pygame.draw.rect(screen, (0, 0, 255), spike_trigger_zone.move(-camera_x, 0), 2)  
 
+
+    # 그리기
     pygame.draw.rect(screen, RED, character_rect.move(-camera_x, 0))
     pygame.display.update()
     clock.tick(60)
